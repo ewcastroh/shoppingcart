d6f6f013c9b228138b891394acc16159834b5162
Le Deng <z00382545@z00382545-mac.local>
2017-04-07 10:56:53 -0400

    latest

--- src/main/java/com/shoppingcart/controllers/HomeController.java
+++ src/main/java/com/shoppingcart/controllers/HomeController.java
@@ -11,8 +11,7 @@ import com.stripe.model.Charge;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
@@ -42,8 +41,16 @@ public class HomeController {
 
         List<CartItem> cartItemList = (List<CartItem>) cartItemRepository.findAll();
 
+        BigDecimal total = new BigDecimal(0);
+
+        for (CartItem item : cartItemList) {
+            total = total.add(item.getSubTotal());
+        }
+
         model.addAttribute("productList", productList);
         model.addAttribute("cartItemList", cartItemList);
+
+        model.addAttribute("total", total.abs());
 
         return "home";
     }
@@ -58,7 +65,7 @@ public class HomeController {
         cartItem.setSubTotal(new BigDecimal(product.getPrice()).setScale(2, BigDecimal.ROUND_HALF_UP));
         cartItemRepository.save(cartItem);
 
-        return "forward:/";
+        return "redirect:/";
     }
 
     @RequestMapping("/remove")
@@ -66,20 +73,34 @@ public class HomeController {
 
         cartItemRepository.delete(id);
 
-        return "forward:/";
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/updateCart", method = RequestMethod.POST)
+    public String updateCart(HttpServletRequest request) {
+        Long id = Long.parseLong(request.getParameter("id"));
+        int qty = Integer.parseInt(request.getParameter("qty"));
+
+        CartItem cartItem =cartItemRepository.findOne(id);
+        cartItem.setQty(qty);
+        cartItem.setSubTotal(new BigDecimal(cartItem.getProduct().getPrice()*qty).setScale(2, BigDecimal.ROUND_HALF_UP));
+
+        cartItemRepository.save(cartItem);
+
+        return "redirect:/";
     }
 
     @RequestMapping("/pay")
     public String pay(HttpServletRequest request, Model model, @RequestParam double total) throws CardException, APIException, AuthenticationException, InvalidRequestException, APIConnectionException {
         // Set your secret key: remember to change this to your live secret key in production
-// See your keys here: https://dashboard.stripe.com/account/apikeys
+        // See your keys here: https://dashboard.stripe.com/account/apikeys
         Stripe.apiKey = "sk_test_p5VUQTAeJjAbqQb6qZJBQDqu";
 
-// Token is created using Stripe.js or Checkout!
-// Get the payment token submitted by the form:
+        // Token is created using Stripe.js or Checkout!
+        // Get the payment token submitted by the form:
         String token = request.getParameter("stripeToken");
 
-// Charge the user's card:
+        // Charge the user's card:
         Map<String, Object> params = new HashMap<String, Object>();
         params.put("amount", (int) total*100);
         params.put("currency", "usd");
@@ -90,7 +111,7 @@ public class HomeController {
 
         Charge charge = Charge.create(params);
 
-        return "forward:/";
+        return "redirect:/";
     }
 
 
--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,31 +1,23 @@
-/**
- * Created by z00382545 on 4/6/17.
- */
-var total = 0;
-
+// /**
+//  * Created by z00382545 on 4/6/17.
+//  */
+//
 $( document ).ready(function() {
 
     $('.item-qty').on('input', function() {
 
         var id = this.id.substring(4);
-        var qty = this.value;
-        var price = $('#price-'+id).text();
 
-        $('#subtotal-'+id).text((price*qty).toFixed(2));
+        $('#update-'+id).css("display", "inline-block");
 
-        $('.subtotal').each(function(){
-            total=total + parseFloat($(this).text());
-        });
 
-        $('#total').text(total);
-        $('#hidden-total').val(total);
 
     });
-
-    $('.subtotal').each(function(){
-        total=total + parseFloat($(this).text());
-    });
-
-    $('#total').text(total);
-    $('#hidden-total').val(total);
+//
+//     $('.subtotal').each(function(){
+//         total=total + parseFloat($(this).text());
+//     });
+//
+//     $('#total').text(total);
+//     $('#hidden-total').val(total);
 });
